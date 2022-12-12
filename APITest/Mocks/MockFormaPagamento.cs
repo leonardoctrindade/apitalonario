3acc5e3dbcd86082486994cecd80175f706c8072
FragasPrisma <andre.fragas@prismafive.com.br>
2022-12-12 16:00:57 -0300

    Alteração para enum FormaPagamento

--- APITest/Mocks/MockFormaPagamento.cs
+++ APITest/Mocks/MockFormaPagamento.cs
@@ -11,21 +11,21 @@ namespace APITest.Mocks
     {
         public static FormaPagamento MontaObjetoUnico()
         {
-            return new FormaPagamento { Id = 1, Descricao = "Teste Mock 1", TipoFormaPagamento = 1, AutorizarDescontos = 1, Conciliacao = true, IdConta = 1 };
+            return new FormaPagamento { Id = 1, Descricao = "Teste Mock 1", TipoFormaPagamento = Tipo.Pix, AutorizarDescontos = 1, Conciliacao = true, IdConta = 1 };
         }
 
         public static FormaPagamento MontaObjetoDescricaoVazio()
         {
-            return new FormaPagamento { Id = 1, Descricao = null, TipoFormaPagamento = 1, AutorizarDescontos = 1, Conciliacao = true, IdConta = 1 };
+            return new FormaPagamento { Id = 1, Descricao = null, TipoFormaPagamento = Tipo.Pix, AutorizarDescontos = 1, Conciliacao = true, IdConta = 1 };
         }
 
         public static List<FormaPagamento> MontaListaItems()
         {
             return new List<FormaPagamento>()
             {
-                new FormaPagamento() {Id = 1, Descricao = "Teste Mock 1", TipoFormaPagamento = 1, AutorizarDescontos = 1, Conciliacao = true, IdConta = 1 },
-                new FormaPagamento() {Id = 2, Descricao = "Teste Mock 2", TipoFormaPagamento = 2, AutorizarDescontos = 2, Conciliacao = true, IdConta = 2 },
-                new FormaPagamento() {Id = 3, Descricao = "Teste Mock 3", TipoFormaPagamento = 3, AutorizarDescontos = 3, Conciliacao = false, IdConta = 3 }
+                new FormaPagamento() {Id = 1, Descricao = "Teste Mock 1", TipoFormaPagamento = Tipo.Pix, AutorizarDescontos = 1, Conciliacao = true, IdConta = 1 },
+                new FormaPagamento() {Id = 2, Descricao = "Teste Mock 2", TipoFormaPagamento = Tipo.Deposito, AutorizarDescontos = 2, Conciliacao = true, IdConta = 2 },
+                new FormaPagamento() {Id = 3, Descricao = "Teste Mock 3", TipoFormaPagamento = Tipo.PrazoFicha, AutorizarDescontos = 3, Conciliacao = false, IdConta = 3 }
             };
         }
     }   
--- Data/Entidades/FormaPagamento.cs
+++ Data/Entidades/FormaPagamento.cs
@@ -18,7 +18,7 @@ namespace Data.Entidades
 
         [Column("TipoFormaPagamento")]
         [Required(ErrorMessage = "Campo de tipo de forma de pagamento não preenchido")]
-        public int TipoFormaPagamento { get; set; }
+        public Tipo TipoFormaPagamento { get; set; }
 
         [Column("AutorizarDescontos")]
         [Required(ErrorMessage = "Campo de autorização de descontos não preenchido")]
@@ -29,5 +29,22 @@ namespace Data.Entidades
 
         [Column("IdConta")]
         public int IdConta { get; set; }
-     }
+    }
+
+    public enum Tipo
+    {
+        Moeda,
+        Cheque,
+        Cartao,
+        PrazoFicha,
+        Convenio,
+        PrazoParcelamento,
+        ConvenioParcelamento,
+        Boleto,
+        Deposito,
+        Pix,
+        TransferenciaBancaria,
+        ProgramaDeFidelidade,
+        Cashback
+    }
 }
