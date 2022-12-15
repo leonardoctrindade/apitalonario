50066d3a9ea5fa01a37305cc518c0c7217d5afa6
FragasPrisma <andre.fragas@prismafive.com.br>
2022-12-15 15:06:06 -0300

    Alteração Entidade Conta Corrente

--- Data/Entidades/ContaCorrente.cs
+++ Data/Entidades/ContaCorrente.cs
@@ -10,16 +10,22 @@ namespace Data.Entidades
     {
         [Column("Id")]
         public int Id { get; set; }
-        [Required]
+
         [Column("NumeroConta")]
+        [Required(ErrorMessage = "Campo de número da conta não preenchido")]
         [MaxLength(15)]
         public string NumeroConta { get; set; }
-        [Required]
+
         [Column("Nome")]
+        [Required(ErrorMessage = "Campo de nome não preenchido")]
         [MaxLength(50)]
         public string Nome { get; set; }
+
         [Column("Limite")]
         [MinLength(0)]
         public double Limite { get; set; }
+
+        [Column("IdFilial")]
+        public int IdFilial { get; set; }
     }
 }
